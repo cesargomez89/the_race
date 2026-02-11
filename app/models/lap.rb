134140b27ed1be0d0cd32fa1a753c1a30c1987e5
Cesar Gomez <cesargomez89@gmail.com>
2026-02-11 12:03:05 -0600

    feat: nested routes for laps inside races > cars > laps

--- app/models/lap.rb
+++ app/models/lap.rb
@@ -4,6 +4,19 @@ class Lap < ApplicationRecord
   has_one :race, through: :race_participant
   has_one :car, through: :race_participant
 
+  before_validation :set_lap_time_ms
+
   validates :lap_number, :start_time, :end_time, :lap_time_ms, presence: true
   validates :lap_number, uniqueness: { scope: :race_participant_id }
+  validate :end_after_start
+
+  private
+
+  def set_lap_time_ms
+    self.lap_time_ms = end_time - start_time
+  end
+
+  def end_after_start
+    errors.add(:end_time, "must be after start time") if end_time < start_time
+  end
 end
